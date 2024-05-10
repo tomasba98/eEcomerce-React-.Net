32560c1a0f10f5513a53ccff8e98ce45a7672462
tomasba98 <tomasbava98@gmail.com>
2024-05-10 18:17:03 -0300

    Created contexts

--- ecomerce.frontend/src/app/layout.tsx
+++ ecomerce.frontend/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SidebarProvider from "@/context/SideBarContext";
+import CartProvider from "@/context/CartContext";
+import ProductProvider from "@/context/ProductContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <SidebarProvider>
+        <CartProvider>
+          <ProductProvider>
+            <body className={inter.className}>{children}</body>
+          </ProductProvider>
+        </CartProvider>
+      </SidebarProvider>
     </html>
   );
 }
